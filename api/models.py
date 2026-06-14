40b92dc2112a59fa2dc8d0f3a108e3136231c63a
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-06-14 22:08:45 +0200

    fixed typo in todo model and migrated DB

--- api/models.py
+++ api/models.py
@@ -5,5 +5,5 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    created_at = models.DateField(add_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
