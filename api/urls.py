2773a8b3cd4c77b9315925f058206d36926caa10
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-06-20 18:57:22 +0200

    created root api urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoListCreateAPIView, TodoRetrieveUpdateDestroyAPIView
+from .views import TodoListCreateAPIView, TodoRetrieveUpdateDestroyAPIView, RootAPIView
 
 urlpatterns = [
-    path('todo/', TodoListCreateAPIView.as_view()),
+    path('', RootAPIView.as_view(), name='root'),
+    path('todo/', TodoListCreateAPIView.as_view(), name='todos'),
     path('todo/<int:pk>/', TodoRetrieveUpdateDestroyAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 class TodoListCreateAPIView(ListCreateAPIView):
     # queryset = Todo.objects.all()
@@ -18,3 +21,10 @@ class TodoRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
+    
+class RootAPIView(APIView):
+
+    def get(self, request):
+        return Response({
+            'todos': reverse('todos', request=request),
+        })
