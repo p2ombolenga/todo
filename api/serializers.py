3afa45069bec83b7ed05c5ef6f42aa615f02773e
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-06-20 15:55:54 +0200

    fix typo in TodoSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Todo
 
-class TodoSerializer(serializers.Modelserializer):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'completed', 'created_at']
-        read_only_filds = ['created_at']
+        read_only_fields = ['created_at']
