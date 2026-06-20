ea02b7ec867d8c3130096744ced44d8d56892a82
p2ombolenga <nsabimanapeter2000@gmail.com>
2026-06-20 21:29:18 +0200

    implement Djangorestframework JWT Authentication

--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.reverse import reverse
 
 class TodoListCreateAPIView(ListCreateAPIView):
     # queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
@@ -18,6 +20,7 @@ class TodoListCreateAPIView(ListCreateAPIView):
 class TodoRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('', include('api.urls')),
 ]
